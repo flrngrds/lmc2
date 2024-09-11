409f2cfab19eb270e207c09ef706971dce0f5e17
flrngrds <flrngrds.pro+1@gmail.com>
2024-09-11 08:54:50 +0200

    Create _document.tsx

---
+++ src/pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="fr">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <title>LaMachineCommerciale - Augmentez vos ventes B2B en automatisant votre prospection.</title>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
